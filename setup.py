15ac815a085555fbee736f1d4c159e56f651fbc3
Bob Jenkins <bob@hasoffers.com>
2016-04-19 18:59:07 -0700

    added basic template

---
+++ setup.py
@@ -0,0 +1,20 @@
+### Go back and revise this once the package is actually written.
+
+ from setuptools import setup
+
+ setup(
+   name='nest',
+   version='0.1.0',
+   author='Bob Jenkins & Marc Teale',
+   author_email='aac@example.com',
+   packages=['package_name', 'package_name.test'],
+   scripts=['bin/script1','bin/script2'],
+   url='http://pypi.python.org/pypi/PackageName/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.txt').read(),
+   install_requires=[
+       "Django >= 1.1.1",
+       "pytest",
+   ],
+)
