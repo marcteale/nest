d37ff81a7ebcafa3b9a5815eea175a9a23bce80f
marcteale <marc.teale@gmail.com>
2016-04-25 20:19:33 -0700

    Initial commit of script.

---
+++ nest/nest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+
+def get_config():
+    '''Look for command line args and config file.  If config is in both,
+    command line args win.'''
+    pass
+
+
+def get_config_from_cli():
+    '''Get configuration from the command line args.'''
+    pass
+
+
+def get_config_from_file():
+    '''Get configuration from the specified configuration file.'''
+    pass
+
+
+def validate_config():
+    '''Validate the configuration created by get_config, exit uncleanly if bad
+    config is specified.'''
+    pass
